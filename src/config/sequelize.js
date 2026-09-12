8611f031abfb15bff37367b3a8b6d7ffa5d724b2
Francisco Molina <155790120+franciscomolinacl@users.noreply.github.com>
2026-09-11 21:14:11 -0300

    Update sequelize.js

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,22 +1,26 @@
-import {
-  Sequelize
-} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const DB_PORT = Number(
-  process.env.DB_PORT || 5432
+// Detectamos si estamos en producción (Render)
+const isProduction = process.env.NODE_ENV === "production";
+
+export const sequelize = new Sequelize(
+  process.env.DATABASE_URL || {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
+  },
+  {
+    dialect: "postgres",
+    logging: console.log,
+    
+    // El SSL solo se activará si está en Render
+    dialectOptions: isProduction ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {} // En local sin SSL
+  }
 );
-
-export const sequelize =
-  new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host:
-        process.env.DB_HOST,
-      port: DB_PORT,
-      dialect:
-        "postgres",
-      logging: console.log
-    }
-  );
